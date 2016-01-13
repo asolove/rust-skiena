5126490ed4d2c4275bdef10a2fb4ae8e6384d4d1
asolove <asolove@gmail.com>
2016-01-12 19:55:53 -0500

    Insertion sort passing basic tests.

--- ch1-intro/src/lib.rs
+++ ch1-intro/src/lib.rs
@@ -42,30 +42,47 @@ pub fn random_vec<R: Rand>(n: i64) -> Vec<R> {
     v
 }
 
-/*
-pub fn insertion_sort<A: Ord, Copy>(items: &mut Vec<A>) {
+fn test_insertion_sort_case<A>(actual: &mut Vec<A>, expected: &Vec<A>) where A: Ord, A: Copy, A: PartialEq {
+    insertion_sort(actual);
+    assert!(actual == expected);
+}
+
+#[test]
+fn test_insertion_sort() {
+    let mut empty: Vec<usize> = vec!();
+    test_insertion_sort_case(&mut empty, &vec!());
+    test_insertion_sort_case(&mut vec!(0), &vec!(0));
+    test_insertion_sort_case(&mut vec!(0, 1), &vec!(0, 1));
+    test_insertion_sort_case(&mut vec!(0, -1), &vec!(-1, 0));
+    test_insertion_sort_case(&mut vec!(5, 1, 2, 4, 3), &vec!(1, 2, 3, 4, 5));
+}
+
+pub fn insertion_sort<A>(items: &mut Vec<A>) where A: Ord, A: Copy {
     let mut val: A;
     for i in 0..items.len() {
         val = items[i];
         for j in 0..i {
             if items[j] > val {
-                shift(&mut items, j, i);
-                items[j] = val;
+                shift(items, j, i);
+                break;
             }
         }
     }
 }
-*/
 
 #[test]
 fn test_shift() {
-    let v: Vec<i32> = vec!(100, 101, 102, 103, 104);
-    shift(&mut v, 2, 5);
-    assert!(v == vec!(100, 101, 102, 102, 103));
+    let mut v: Vec<i32> = vec!(100, 101, 102, 103, 104);
+    shift(&mut v, 2 as usize, 4 as usize);
+    assert!(v == vec!(100, 101, 104, 102, 103));
 }
 
-fn shift<A: Ord, Copy>(items: &mut Vec<A>, start: usize, end: usize) {
+// Moves the elements from start to end one slot to the right,
+// and puts the element in end at start.
+fn shift<A: Copy>(items: &mut Vec<A>, start: usize, end: usize) {
+    let end_item = items[end];
     for i in (start..end).rev() {
-        items[i] = items[i-1];
+        items[i+1] = items[i];
     }
+    items[start] = end_item;
 }
